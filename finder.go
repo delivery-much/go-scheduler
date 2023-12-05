0c406c0e30332b72dd91d4d4c699433d9a62e7a1
lucas4ndrade <lucasandrade664@gmail.com>
2023-12-05 17:08:32 -0300

    improve finder documentation

--- finder.go
+++ finder.go
@@ -1,5 +1,8 @@
 package scheduler
 
+// Finder its a helper struct used to pass parameters to the database List action.
+//
+// Currently, only the status, name or data values can be used.
 type Finder struct {
 	Status string
 	Name   string
